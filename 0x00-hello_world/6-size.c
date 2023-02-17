88af47eb717980cd599d5a8c238f885561e24660
Carol <wanjikucarol028@gmail.com>
2023-02-17 03:11:06 -0800

    size.c.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,12 +14,14 @@ int main(void) /**
 	long long int d;
 	float f;
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 /**
- *Return: (0);
+ * main - finction that returns the variable values
+ *Return: Always 0 (Success)
  */
+return (0);
 }
-gcc 6 - size.c - m32 - o size32 2 > / tmp / 32
+
