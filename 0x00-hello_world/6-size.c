4dfb63bdf23ef176f5503014dfdabfe806a26012
Carol <wanjikucarol028@gmail.com>
2023-02-17 01:44:14 -0800

    6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,18 +3,22 @@
  * main - A program that prints the size of various computer types
  * Return: 0 (Success)
  */
-int main(void)
+int main(void) /**
+	*main function - Function allows initialization and printing of variables
+	*/
+
 {
 	char a;
 	int b;
 	long int c;
 	long long int d;
-	float e;
+	float f;
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
-return (0);
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+/**
+ *Return (0);
+ */
 }
-gcc 6 - size.c
