eca43734229ee9031dcc87664b990855f398ff9b
Carol <wanjikucarol028@gmail.com>
2023-02-17 14:48:21 -0800

    print

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * Main - Entry points
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	(c <= 'z') {
+		if ((c != 'q' && c != 'e') && c <= 'z')
+			putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
