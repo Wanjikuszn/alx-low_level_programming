7b9519fdbd899888e75efc1f4617f4714a802317
Carol <wanjikucarol028@gmail.com>
2023-02-17 17:15:37 -0800

    print numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,12 +3,12 @@
  * Main - Entry points
  * Return: Always 0 (Success)
  */
-int main:(void)
+int main(void)
 { 
 	int a;
 
 	for (a = 0; a < 10; a++)
 		printf("%d", a);
-	putchar ('\n');
+	putchar('\n');
 	return (0);
 }
